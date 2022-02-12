5ae60106fe87c1c6d6491b6810511db5d1a5b4fb
Saklain Mustak <saklainmustak@sakmac.local>
2022-02-12 12:19:32 +0600

    added log out button

--- banking.js
+++ banking.js
@@ -7,19 +7,30 @@ function getInputValue(inputId){
     inputField.value = '';
     return amountValue;
 }
+    //update balance
 function updateTotalField(totalFieldid,newDepositAmount){
    //debugger;
    const totalElement = document.getElementById(totalFieldid);
    const totalText = totalElement.innerText;
    const previousTotal = parseFloat(totalText);
-   //const newDepositTotal = previousDepositAmoutn + newDepositAmount;
    totalElement.innerText = previousTotal + newDepositAmount;
    return totalElement;
 }
-function updateBalance(balanceAmount, isAdd){
+   // get current balance
+function getCurrentBalance(){
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceText = balanceTotal.innerText;
     const previousBalance = parseFloat(previousBalanceText);
+    return previousBalance;
+}
+
+   // handle withdraw button
+function updateBalance(balanceAmount, isAdd){
+    const balanceTotal = document.getElementById('balance-total');
+    /* 
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalance = parseFloat(previousBalanceText); */
+    const previousBalance = getCurrentBalance();
     if(isAdd == true){
         balanceTotal.innerText = previousBalance +  balanceAmount;
     }
@@ -30,25 +41,41 @@ function updateBalance(balanceAmount, isAdd){
 
 }
 
+// logout btn
+ document.getElementById('logout-btn').addEventListener('click',function(){
+    window.location.href = 'index.html';
+ })
 // Deposit button
 document.getElementById('deposit-btn').addEventListener('click',function(){
    
    // get deposit amount
    const newDepositAmount = getInputValue('deposit-input');
    // deposit total
-   const  depositTotal =  updateTotalField('deposit-total',newDepositAmount);
-   //update balance
-   updateBalance(newDepositAmount, true);
+   if(newDepositAmount > 0){
+    updateTotalField('deposit-total',newDepositAmount);
+    //update balance
+    updateBalance(newDepositAmount, true);
+   }
+   else{
+       alert('Inavalid amount');
+   }
+
 });
 
 // withdraw button
 document.getElementById('withdraw-btn').addEventListener('click',function(){
+    const currenBalance = getCurrentBalance();
 
     // get withdraw ampunt
-    const withdrawInputAmount = getInputValue('withdraw-input')
+    const withdrawInputAmount = getInputValue('withdraw-input');
+    if(withdrawInputAmount > 0 && withdrawInputAmount <= currenBalance){
     // get withdraw total
-    const withdrawAmount = updateTotalField('withdraw-total',withdrawInputAmount);
+    updateTotalField('withdraw-total',withdrawInputAmount);
     //updat Balance
-   updateBalance(withdrawInputAmount , false);
-
+    updateBalance(withdrawInputAmount , false);
+   }
+   else{
+       alert('Give valid number');
+   }
+ 
 });
